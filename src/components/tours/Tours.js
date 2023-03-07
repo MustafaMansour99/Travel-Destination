dd64a7feba0018908a056a750332967902e861a5
abostaf <23037172@student.ltuc.com>
2023-03-07 14:38:59 +0000

    lab 16 done

---
+++ src/components/tours/Tours.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../components/tours/Tours.css'
+const data = require('../../data/db.json')
+function Tours() {
+    return (
+        <div className='tour'>    
+                <h2>Travel list:</h2>
+            {data.map((item) => {
+                return(
+                    <div key={item.ID}>
+                    <h3 className='name'>{item.name}</h3>
+                    <img src={item.image} className='image' alt={item.name}></img>
+                </div>
+
+                )
+            })}
+        </div>
+    )
+}
+export default Tours;
